9303bd436e5ae81d5c85b93ed70c4260a359312f
Franco Colon <potter.f.22.4.98@gmail.com>
2020-08-09 18:32:57 -0300

    changes in views

--- controllers/modules/userCatcher.js
+++ controllers/modules/userCatcher.js
@@ -6,24 +6,28 @@ const userCatcher = {
     // if no user is in cookies or session, return false
     recordUser: async function (req, res) {
         // Validate content
-        console.log('about to start with the catcher');
+        console.log("about to start with the catcher");
         console.log(`mw: checking session/cookies ${req.session.userId} - ${req.cookies.userId}`);
         if (!req.session.userId && !req.cookies.userId) {
             console.log("no cookies or session");
             return false;
         }
         let credentials; // assign credentials
-        req.session.userId ? (credentials = req.session.userId) : (credentials = req.cookies.userId);
+        req.session.userId
+            ? (credentials = req.session.userId)
+            : (credentials = req.cookies.userId);
         if (!isNaN(credentials)) {
             let user = await Usuarios.findByPk(credentials);
             return {
                 userType: Number(user.id_permiso),
                 userName: user.nombre_de_usuario,
-             };
+            };
         }
-        console.log('false :(');
+        console.log("false :(");
         return false;
-        }
-    }
+    },
+};
+
+module.exports = userCatcher;
 
 module.exports = userCatcher;
